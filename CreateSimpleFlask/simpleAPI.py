0527099da4f00b32031d6ed62033aa6af4efdf79
hackerFaruk <onuralparslan97@gmail.com>
2025-04-20 22:06:50 +0300

    updated

--- CreateSimpleFlask/simpleAPI.py
+++ CreateSimpleFlask/simpleAPI.py
@@ -11,6 +11,18 @@ def home():
 def second_page():
     return render_template('second.html')
 
+@app.route('/name')
+def name_page():
+    return "Ata"
+
+@app.route('/double/<sayi>')
+def double(sayi):
+    return sayi*2
+
+@app.route('/doublesayi/<sayi>')
+def doublesayi(sayi):
+    return str(int(sayi)*2)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
